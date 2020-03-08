d57320730cea110e55a48e5daf5419031e5f5fbc
James Oxley-Shaw <jamesoxley-shaw@Nialls-MacBook.local>
2020-03-08 20:46:44 +0000

    renamed map class for clarity

---
+++ src/components/Mapview.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Mapview extends Component {
+  render() {
+    return (
+      <section>
+        Mapview component.
+      </section>
+    );
+  }
+}
+ 
+export default Mapview;
