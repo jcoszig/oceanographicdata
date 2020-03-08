5eeeb8e8785e4bbd6b6377ef11d77e41c63b154e
James Oxley-Shaw <jamesoxley-shaw@Nialls-MacBook.local>
2020-03-08 20:47:50 +0000

    initialized state -> data keys in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
  
 class App extends Component {
+
+  // Initialize state
+  state = {
+    dataProperties : {
+      type : {},
+      temporal : {},
+      publisherName : {}, //publisher_name
+      publisherType : {}, //publisher_type
+      modified : {},
+      identifier : {},
+      contactpointHasemail : {}, //contactpoint_hasemail
+      contactpointFn : {}, //contactpoint_fn
+      contactpointType : {}, //contactpoint_type
+      accrualperiodicity : {},
+      accesslevel : {},
+      spatial : {},
+      title : {},
+      description : {}
+    }
+  }
+
   render() {
     return (
       <div>
