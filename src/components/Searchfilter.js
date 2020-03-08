40f5ad340aad9e8ba86cd830ddf53ce3a954f429
James Oxley-Shaw <jamesoxley-shaw@Nialls-MacBook.local>
2020-03-08 21:06:36 +0000

    renamed search -> searchfilter for clarity. Added state update from user input in search bar

---
+++ src/components/Searchfilter.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+ 
+class SearchFilter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchQuery: ""
+    }
+  };
+
+  /* Update local state with user input */
+  handleChange = (e) => {
+    this.setState({
+      searchQuery: e.target.value
+    })
+    this.props.onChange(event.target.value)
+  };
+
+  render() {
+    return (
+      <section className="searchbar-wrapper">  
+        <label for="searchbar" className="">Filter by:</label>
+        <input type="search"
+          className=""  
+          id="" 
+          name="searchbar" 
+          placeholder="" 
+          value={this.state.searchQuery} 
+          autocomplete="off"/>
+      </section>
+    );
+  }
+}
+
+export default SearchFilter;
