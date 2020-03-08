8a6cd28b29a0e06b87238cb53ba52d1ddcb9d636
James Oxley-Shaw <jamesoxley-shaw@Nialls-MacBook.local>
2020-03-08 20:49:49 +0000

    initial search bar markup

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,18 @@ import React, { Component } from "react";
 class Search extends Component {
   render() {
     return (
-      <div>
-        Search component.
-      </div>
+      <section className="searchbar-wrapper">  
+        <label for="searchbar" className=""></label>
+        <input type="search"
+         id="" 
+         name="searchbar" 
+         placeholder="" 
+         value="" 
+         className="" 
+         autocomplete="off"/>
+      </section>
     );
   }
 }
- 
+
 export default Search;
