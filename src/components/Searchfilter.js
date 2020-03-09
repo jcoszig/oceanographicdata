085f428355c433b0a7f5120480305f402fec4544
James Oxley-Shaw <jamesoxley-shaw@Nialls-MacBook.local>
2020-03-09 17:36:14 +0000

    debouncing added to search filter, debounce added as helper function

--- src/components/Searchfilter.js
+++ src/components/Searchfilter.js
@@ -1,30 +1,17 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import {debounce} from "../helpers";
  
 class SearchFilter extends Component {
-
   // Typecheck 
   static propTypes = {
     updateSearchQuery: PropTypes.func
   }
-  // handleChange = e => {
 
-  //   // this.props.onChange(e.target.value)
-  //   const updatedSearchQuery = {
-  //     ...this.props.searchQuery,
-  //     [e.currentTarget.name]: e.currentTarget.value
-  //   };
-  //   // update state
-  //   this.props.updateSearchQuery(updatedSearchQuery);
-  // };
-
-  handleChange = e => {
-    let searchQuery = e.target.value;
-    let timeout = null;
-
+  handleChange = debounce(searchTerm => {
     /* Begin filtering once string is > 3 char */
-    return searchQuery.length > 3 ? this.props.updateSearchQuery(searchQuery) : false;
-  };
+    return searchTerm.length > 3 ? this.props.updateSearchQuery(searchTerm) : false;
+  }, 500)
 
   render() {
     return (
@@ -34,7 +21,7 @@ class SearchFilter extends Component {
           className=""  
           id="" 
           name="searchbar" 
-          onChange={this.handleChange}
+          onChange={e => {this.handleChange(e.target.value)}}
           autoComplete="off"/>
       </section>
     );
