cec1db88b6960380e6f5c36a2c5717f9c108c86a
James Oxley-Shaw <jamesoxley-shaw@Nialls-MacBook.local>
2020-03-12 12:03:52 +0000

    added helper class for formating temporal date format

--- src/helpers.js
+++ src/helpers.js
@@ -9,8 +9,22 @@ export function formatDate(date){
     return;
 }
 
-export function formatDateRange(date){
-    
-    //todo
-    return;
+/* 
+    Convert temporal into more readable format. Strips H,M,S 
+    Expects: string - temporal in format: 'YYYY-MM-DDTHH:MM:SS/YYYY-MM-DDTHH:MM:SS'
+    Returns: string - format: DD-MM-YY to DD-MM-YY
+*/
+export function formatDateRangeUK(date){
+    const fromTo = (date.split('/'))
+    const dateFrom = format(fromTo[0]);
+    const dateTo = format(fromTo[1]);
+
+    function format(item){
+        const separateTime = item.split('T')
+        const YMD = separateTime[0].split('-')
+        return `${YMD[2]}-${YMD[1]}-${YMD[0]}`;
+    }
+    return dateFrom + ' to ' + dateTo;
 }
+
+
