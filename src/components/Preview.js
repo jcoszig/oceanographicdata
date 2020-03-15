f0215b671d711750dd64829c648394677953b0a7
James Oxley-Shaw <jamesoxley-shaw@nialls-macbook.lan>
2020-03-15 13:47:31 +0000

    attempt at trimming description after first match

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -10,6 +10,18 @@ class Preview extends Component {
       splitStr.toLowerCase() === searchQuery.toLowerCase() ? <span className="highlight">{splitStr}</span> : splitStr)}</p>;
   }
   
+  // trimDescription = (str, searchQuery) => {
+  //   const description = this.highlightMatches(str, searchQuery);
+
+  //   // isolate description after matched word up until end of sentence.
+  //   const upUntilLastSentence = new RegExp('[^.]+');
+  //   const afterMatchedWord = description.props.children[2] ? description.props.children[2] : false;
+  //   if(afterMatchedWord){
+  //     console.log(afterMatchedWord.match(upUntilLastSentence));
+  //   }
+  //   return;
+  // }
+
   render() {    
 
     return (
@@ -19,6 +31,7 @@ class Preview extends Component {
         </div>
         <h4 className="preview-title">{this.highlightMatches(this.props.title, this.props.searchquery)}</h4>
         <div className="preview-description">{this.highlightMatches(this.props.description, this.props.searchquery)}</div>
+        {/* <div className="preview-description">{this.trimDescription(this.props.description, this.props.searchquery)}</div> */}
         <div className="preview-date-published">Date: {formatDateRangeUK(this.props.temporal)}</div>
         <Link to={`/article/${this.props.index}`}>
           <div  className="preview-view-full">View full details</div>
